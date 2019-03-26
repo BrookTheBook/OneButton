b1132d5df022f253241f9c3f884408e9387c7574
LeithBA <leith.benabdessalem@ue-germany.de>
2019-03-27 00:00:22 +0100

    Unity project setup + Base layout + SwordLoop script added

---
+++ Assets/Scripts/PlayerLoop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoop : MonoBehaviour {
+
+	[SerializeField] GameObject sword;
+	[SerializeField] float attackAngle;
+
+	private float distance;
+
+	void Start () 
+	{
+		sword = transform.Find("Sword").gameObject;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Attack();
+	}
+
+	void Attack()
+	{
+		distance = Mathf.Abs(attackAngle - sword.transform.rotation.z);
+		Debug.Log(distance);
+		if (distance > 0.1f)
+			{
+				
+				sword.transform.RotateAround(sword.transform.position, sword.transform.forward, Mathf.LerpAngle(sword.transform.rotation.z, attackAngle, 0.001f));
+			}
+	}
+}
